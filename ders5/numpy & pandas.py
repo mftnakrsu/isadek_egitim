0c1f8d7c591c37cdd072ea3e9c90d8e9b4215488
Meftun AKARSU <57320216+mftnakrsu@users.noreply.github.com>
2023-07-05 19:56:54 +0300

    Add files via upload

---
+++ ders5/numpy & pandas.py
@@ -0,0 +1,464 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[27]:
+
+
+# Numpy & Pandas
+
+
+# In[29]:
+
+
+import numpy as np
+a = [1, 2, 3, 4]
+b = [2, 3, 4, 5]
+
+ab = []
+
+for i in range(0, len(a)):
+    ab.append(a[i] * b[i])
+
+a = np.array([1, 2, 3, 4])
+b = np.array([2, 3, 4, 5])
+a * b
+
+#############################################
+# NumPy Array'i Oluşturmak (Creating Numpy Arrays)
+#############################################
+import numpy as np
+
+np.array([1, 2, 3, 4, 5])
+type(np.array([1, 2, 3, 4, 5]))
+np.zeros(10, dtype=int)
+np.random.randint(0, 10, size=10)
+np.random.normal(10, 4, (3, 4))
+
+#############################################
+# NumPy Array Özellikleri (Attibutes of Numpy Arrays)
+#############################################
+import numpy as np
+
+# ndim: boyut sayısı
+# shape: boyut bilgisi
+# size: toplam eleman sayısı
+# dtype: array veri tipi
+
+a = np.random.randint(10, size=5)
+a.ndim
+a.shape
+a.size
+a.dtype
+
+#############################################
+# Yeniden Şekillendirme (Reshaping)
+#############################################
+import numpy as np
+
+np.random.randint(1, 10, size=9)
+np.random.randint(1, 10, size=9).reshape(3, 3)
+
+ar = np.random.randint(1, 10, size=9)
+ar.reshape(3, 3)
+
+
+#############################################
+# Index Seçimi (Index Selection)
+#############################################
+import numpy as np
+a = np.random.randint(10, size=10)
+a[0]
+a[0:5]
+a[0] = 999
+
+m = np.random.randint(10, size=(3, 5))
+
+m[0, 0]
+m[1, 1]
+m[2, 3]
+
+m[2, 3] = 999
+
+m[2, 3] = 2.9
+
+m[:, 0]
+m[1, :]
+m[0:2, 0:3]
+
+#############################################
+# Fancy Index
+#############################################
+import numpy as np
+
+v = np.arange(0, 30, 3)
+v[1]
+v[4]
+
+catch = [1, 2, 3]
+
+v[catch]
+
+#############################################
+# Numpy'da Koşullu İşlemler (Conditions on Numpy)
+#############################################
+import numpy as np
+v = np.array([1, 2, 3, 4, 5])
+
+#######################
+# Klasik döngü ile
+#######################
+ab = []
+for i in v:
+    if i < 3:
+        ab.append(i)
+
+#######################
+# Numpy ile
+#######################
+v < 3
+
+v[v < 3]
+v[v > 3]
+v[v != 3]
+v[v == 3]
+v[v >= 3]
+
+#############################################
+# Matematiksel İşlemler (Mathematical Operations)
+#############################################
+import numpy as np
+v = np.array([1, 2, 3, 4, 5])
+
+v / 5
+v * 5 / 10
+v ** 2
+v - 1
+
+np.subtract(v, 1)
+np.add(v, 1)
+np.mean(v)
+np.sum(v)
+np.min(v)
+np.max(v)
+np.var(v)
+v = np.subtract(v, 1)
+
+#######################
+# NumPy ile İki Bilinmeyenli Denklem Çözümü
+#######################
+
+# 5*x0 + x1 = 12
+# x0 + 3*x1 = 10
+
+a = np.array([[5, 1], [1, 3]])
+b = np.array([12, 10])
+
+np.linalg.solve(a, b)
+
+
+# In[30]:
+
+
+#############################################
+# PANDAS
+#############################################
+
+# Pandas Series
+# Veri Okuma (Reading Data)
+# Veriye Hızlı Bakış (Quick Look at Data)
+# Pandas'ta Seçim İşlemleri (Selection in Pandas)
+# Toplulaştırma ve Gruplama (Aggregation & Grouping)
+# Apply ve Lambda
+# Birleştirme (Join) İşlemleri
+
+#############################################
+# Pandas Series
+#############################################
+import pandas as pd
+
+s = pd.Series([10, 77, 12, 4, 5])
+type(s)
+s.index
+s.dtype
+s.size
+s.ndim
+s.values
+type(s.values)
+s.head(3)
+s.tail(3)
+
+
+# In[32]:
+
+
+#############################################
+# Veri Okuma (Reading Data)
+#############################################
+import pandas as pd
+
+df = pd.read_csv("datasets/advertising.csv")
+df.head()
+# pandas cheatsheet
+
+
+# In[33]:
+
+
+df = sns.load_dataset("titanic")
+
+
+# In[34]:
+
+
+df.head()
+
+
+# In[35]:
+
+
+df.tail()
+
+
+# In[37]:
+
+
+df.head()
+df.tail()
+df.shape
+df.info()
+df.columns
+df.index
+df.describe().T
+df.isnull().values.any()
+df.isnull().sum()
+df["sex"].head()
+df["sex"].value_counts()
+
+
+import pandas as pd
+import seaborn as sns
+df = sns.load_dataset("titanic")
+df.head()
+
+df.index
+df[0:13]
+df.drop(0, axis=0).head()
+
+delete_indexes = [1, 3, 5, 7]
+df.drop(delete_indexes, axis=0).head(10)
+
+
+df["age"].head()
+df.age.head()
+
+df.index = df["age"]
+
+df.drop("age", axis=1).head()
+
+df.drop("age", axis=1, inplace=True)
+df.head()
+
+df.index
+
+df["age"] = df.index
+
+df.head()
+df.drop("age", axis=1, inplace=True)
+
+df.reset_index().head()
+df = df.reset_index()
+df.head()
+
+import pandas as pd
+import seaborn as sns
+df = sns.load_dataset("titanic")
+df.head()
+
+"age" in df
+
+df["age"].head()
+df.age.head()
+
+df["age"].head()
+type(df["age"].head())
+
+
+df[["age"]].head()
+type(df[["age"]].head())
+
+df[["age", "alive"]]
+
+col_names = ["age", "adult_male", "alive"]
+df[col_names]
+
+df["age2"] = df["age"]**2
+df["age3"] = df["age"] / df["age2"]
+
+df.drop("age3", axis=1).head()
+
+df.drop(col_names, axis=1).head()
+
+df.loc[:, ~df.columns.str.contains("age")].head()
+
+
+import pandas as pd
+import seaborn as sns
+pd.set_option('display.max_columns', None)
+df = sns.load_dataset("titanic")
+df.head()
+
+df.iloc[0:3]
+df.iloc[0, 0]
+
+df.loc[0:3]
+
+df.iloc[0:3, 0:3]
+df.loc[0:3, "age"]
+
+col_names = ["age", "embarked", "alive"]
+df.loc[0:3, col_names]
+
+
+import pandas as pd
+import seaborn as sns
+pd.set_option('display.max_columns', None)
+df = sns.load_dataset("titanic")
+df.head()
+
+df[df["age"] > 50].head()
+df[df["age"] > 50]["age"].count()
+
+df.loc[df["age"] > 50, ["age", "class"]].head()
+
+df.loc[(df["age"] > 50) & (df["sex"] == "male"), ["age", "class"]].head()
+
+df["embark_town"].value_counts()
+
+df_new = df.loc[(df["age"] > 50) & (df["sex"] == "male")
+       & ((df["embark_town"] == "Cherbourg") | (df["embark_town"] == "Southampton")),
+       ["age", "class", "embark_town"]]
+
+df_new["embark_town"].value_counts()
+
+# - count()
+# - first()
+# - last()
+# - mean()
+# - median()
+# - min()
+# - max()
+# - std()
+# - var()
+# - sum()
+# - pivot table
+
+import pandas as pd
+import seaborn as sns
+pd.set_option('display.max_columns', None)
+df = sns.load_dataset("titanic")
+df.head()
+
+df["age"].mean()
+
+df.groupby("sex")["age"].mean()
+
+df.groupby("sex").agg({"age": "mean"})
+df.groupby("sex").agg({"age": ["mean", "sum"]})
+
+df.groupby("sex").agg({"age": ["mean", "sum"],
+                       "survived": "mean"})
+
+
+df.groupby(["sex", "embark_town"]).agg({"age": ["mean"],
+                       "survived": "mean"})
+
+df.groupby(["sex", "embark_town", "class"]).agg({"age": ["mean"],
+                       "survived": "mean"})
+
+
+df.groupby(["sex", "embark_town", "class"]).agg({
+    "age": ["mean"],
+    "survived": "mean",
+    "sex": "count"})
+
+
+
+import pandas as pd
+import seaborn as sns
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+df = sns.load_dataset("titanic")
+df.head()
+
+df["age2"] = df["age"]*2
+df["age3"] = df["age"]*5
+
+(df["age"]/10).head()
+(df["age2"]/10).head()
+(df["age3"]/10).head()
+
+for col in df.columns:
+    if "age" in col:
+        print(col)
+
+for col in df.columns:
+    if "age" in col:
+        print((df[col]/10).head())
+
+for col in df.columns:
+    if "age" in col:
+        df[col] = df[col]/10
+
+df.head()
+
+df[["age", "age2", "age3"]].apply(lambda x: x/10).head()
+
+df.loc[:, df.columns.str.contains("age")].apply(lambda x: x/10).head()
+
+df.loc[:, df.columns.str.contains("age")].apply(lambda x: (x - x.mean()) / x.std()).head()
+
+def standart_scaler(col_name):
+    return (col_name - col_name.mean()) / col_name.std()
+
+df.loc[:, df.columns.str.contains("age")].apply(standart_scaler).head()
+
+
+df.loc[:, df.columns.str.contains("age")] = df.loc[:, df.columns.str.contains("age")].apply(standart_scaler)
+
+df.head()
+
+import numpy as np
+import pandas as pd
+m = np.random.randint(1, 30, size=(5, 3))
+df1 = pd.DataFrame(m, columns=["var1", "var2", "var3"])
+df2 = df1 + 99
+
+pd.concat([df1, df2])
+
+pd.concat([df1, df2], ignore_index=True)
+
+
+
+df1 = pd.DataFrame({'employees': ['john', 'dennis', 'mark', 'maria'],
+                    'group': ['accounting', 'engineering', 'engineering', 'hr']})
+
+df2 = pd.DataFrame({'employees': ['mark', 'john', 'dennis', 'maria'],
+                    'start_date': [2010, 2009, 2014, 2019]})
+
+pd.merge(df1, df2)
+pd.merge(df1, df2, on="employees")
+
+# Her çalışanın müdürünün bilgisine erişmek istiyoruz.
+df3 = pd.merge(df1, df2)
+
+df4 = pd.DataFrame({'group': ['accounting', 'engineering', 'hr'],
+                    'manager': ['Caner', 'Mustafa', 'Berkcan']})
+
+pd.merge(df3, df4)
+
+
+# In[ ]:
+
+
+
+
